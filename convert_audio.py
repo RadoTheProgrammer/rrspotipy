498757ca50bd130143af7085eb9d35447014f88a
RadoTheProgrammer <rado@arazakar.com>
2024-03-03 17:28:53 +0100

    tried convert_audio.py

---
+++ convert_audio.py
@@ -0,0 +1,10 @@
+import rrytapi
+import os
+#JVKE Autumn url: https://youtube.com/watch?v=QegcGsE9tYE
+DST_DIR="/Volumes/RRP515/Rsongs"
+#INPUT_FILE="playlist_songs/JVKE---this-is-what-autumn-feels-like_251.weba.mp3"
+INPUT_FILE="rrytapi_downloads/JVKE_-_this_is_what_autumn_feels_like_140.m4a"
+rrytapi.convert_audio(
+    INPUT_FILE,
+    os.path.join(DST_DIR,os.path.basename(INPUT_FILE))+".wav",
+    ext="wav")
